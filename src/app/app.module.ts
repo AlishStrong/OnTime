c15ec34e97e7bdd88175f860f22af763256156d7
Alisher Aliev <alisher.aliev@tietoevry.com>
2024-09-12 14:14:40 +0300

    Create bases for the public Landing and Login pages: - Create Shared module: ng generate module shared; - Create NavBar component that will be shared: ng generate component shared/navbar -m shared; - Create Landing Page component: ng generate component pages/landing-page; - Create Login Page component: ng generate component pages/login-page;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,20 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { SharedModule } from './shared/shared.module';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LandingPageComponent, LoginPageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot(),
     EffectsModule.forRoot([]),
     StoreRouterConnectingModule.forRoot(),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
